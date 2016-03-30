0adb5d9037ae5cf3ccc80f489457a941efa117ab
neliSun <nelson.chow91.nc@gmail.com>
2016-03-30 11:24:57 -0700

    Initial Commit

---
+++ EveryDoP2/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  EveryDoP2
+//
+//  Created by Nelson Chow on 2016-03-30.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
